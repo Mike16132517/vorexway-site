b29cd02863cce52726b35881c21124d48f48ac99
Mike16132517 <mish.ganiew@yandex.ru>
2026-08-26 00:29:09 +0300

    Add Cloudflare Turnstile site key

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.VOREXWAY_CONFIG = {
   // Public Turnstile site key. This is NOT a secret.
   // Replace this value after creating the Turnstile widget in Cloudflare.
-  turnstileSiteKey: "PASTE_TURNSTILE_SITE_KEY_HERE"
+  turnstileSiteKey: "0x4AAAAAAEcHMTyq_5ylV81Y"
 };
